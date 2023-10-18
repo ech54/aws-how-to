91e222c754a005441dd1ce2b7a6ee559728954db
ech54 <ech.charton@gmail.com>
2023-10-18 13:10:33 +0200

    include first project on aws

---
+++ tf/tf-aws-get-started/learn-tf-with-aws-ec2/variables-ec2.tf
@@ -0,0 +1,38 @@
+
+variable "directory_id" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+variable "app_name" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+variable "app_environment" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+variable "instance_type" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+variable "private_subnet_1_id" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+variable "private_subnet_2_id" {
+  description = "CHANGE ME"
+  type        = string
+  default     = "CHANGE ME"
+}
+
+
